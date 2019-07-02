e482ce0d75bec4235feb458c61b2b5b78b89b3c3
shashank <shashu1999@gmail.com>
2019-07-02 12:46:52 +0530

    Multithread

--- dataupdate.py
+++ dataupdate.py
@@ -1,10 +1,11 @@
 import time
 import math
+import random
 
 i = 0
 while(True):
     f = open("eegdata.txt","a")
-    f.write("{},{}\n".format(i, pow(2,i)))
+    f.write("{},{}\n".format(i, math.sin(i)))
     f.close()    
-    time.sleep(1)
+    time.sleep(0.001)
     i = i + 1

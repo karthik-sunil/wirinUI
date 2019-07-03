cd8e613d7173205a6179cdddf81c212b37d6cda6
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2019-07-03 13:51:20 +0530

    new

--- restartQT.py
+++ restartQT.py
@@ -2,21 +2,47 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sys
 
+import serial
+
+def findComPorts(menu):
+    menu = menu
+    print("Hello")
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    result = []
+    print(menu)
+    for port in ports:
+        
+        try:
+            s = serial.Serial(port)
+            s.close()
+            print(type(port))
+            result.append(port)
+            
+        except (OSError, serial.SerialException):
+            pass
+       
+    if len(result) == 0:
+        menu.addAction("NO PORTS")
+    for r in result:
+        menu.addAction(r)
 
 
 app =QApplication(sys.argv)
 window = QMainWindow()
 window.setGeometry(50,50,900,500)
 mainLayout = QVBoxLayout()
-topBar = window.addToolBar("File")
-new = QAction(QIcon(""),"new",window)
+topBar = QToolBar()
+window.addToolBar(topBar)
 
+comPorts = QToolButton()
+comPorts.setText("COM")
+menu = QMenu()
+findComPorts(menu)
 
-viewStatAct = QAction('View statusbar', window, checkable=True)
-viewStatAct.setStatusTip('View statusbar')
-viewStatAct.setChecked(True)
-
-topBar.addAction(new)
+comPorts.setMenu(menu)
+comPorts.setPopupMode(QToolButton.InstantPopup)
+topBar.addWidget(comPorts)
 
 window.setLayout(mainLayout)
 window.show()

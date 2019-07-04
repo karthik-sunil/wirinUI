792c3c64625066ecb051e062ae5b4df85b6b3c16
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2019-07-04 12:07:24 +0530

    7/4 12:07

--- GUI_test_qt.1.py
+++ GUI_test_qt.1.py
@@ -10,22 +10,42 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 from functools import partial 
-def start_read():
-    print("start reading")
+import threading
+running = False
 
+
+def start_read():
+    global running
+    print("start reading")
+    running = True
+    while(running):
+        print(running)
+t1 = threading.Thread(target=start_read, args=()) 
 def stop_read():
     print("stop reading")
+    running = False
 
 def plot_data():
     print("plot")
 
 def display(a):
+    global running
+    global t1
     case = a.text()
     print (case)
+   
+        
     if case == "Start":
-        start_read()
+        #start_read()
+        
+        t1.start()
     elif case == "Stop":
-        stop_read()
+        running = False
+        
+        t1.join()
+        
+        #stop_read()
+
     elif case == "Plot":
         plot_data()
 
@@ -43,63 +63,154 @@ def animate(i):
     a.plot(xList,yList)
 
 
+def findComPorts(menu):
+    menu = menu
+    menu.clear()
+    print("Hello")
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+    result = []
+    print(ports)
+    #print(menu)
+    for port in ports:
+        
+        try:
+            s = serial.Serial(port)
+            s.close()
+            #print(type(port))
+            result.append(port)
+            
+        except (OSError, serial.SerialException):
+            pass
+     
+    
+    #result = ["COM1","COM2"]
+    if len(result) == 0:
+        action = menu.addAction("NO PORTS")
+        action.setEnabled(False)
+    for r in result:
+        r = menu.addAction(r)
+    #menu.triggered.disconnect()
+    #menu.triggered.connect(partial(setComPort, menu))
+
+def setComPort(menu,a):
+    #menu.clear()
+
+    print(a.text())
+    
+    findComPorts(menu)
+   
 
 
 app = QApplication(sys.argv)
-window = QMainWindow()
+mainWindow = QMainWindow()
+mainWindow.setGeometry(50, 50, 500, 500)
+mainWindow.setWindowTitle("Readings")
+
 wid = QWidget()
-window.setCentralWidget(wid)
-
-
-
-
-window.setGeometry(0, 0, 500, 500)
+mainWindow.setCentralWidget(wid)
 hmainBox = QHBoxLayout()
-v_layout = QVBoxLayout()
-toolbar = QToolBar()
-window.addToolBar(toolbar)
-comport = QToolButton()
-comport.setText("COM")
-menu = QMenu()
-menu.addAction("COM1",)
-menu.addAction("COM3",)
-menu.triggered.connect()
-comport.setMenu(menu)
-comport.setPopupMode(QToolButton.InstantPopup)
-toolbar.addWidget(comport)
-start = QAction(QIcon(),"Start", window)
-toolbar.addAction(start)
-stop  = QAction(QIcon(),"Stop", window)
-toolbar.addAction(stop)
-plot  = QAction(QIcon(),"Plot", window)
-toolbar.addAction(plot)
-toolbar.actionTriggered[QAction].connect(display)
-figure = Figure()
-canvas = FigureCanvas(figure)
-canvas2 = FigureCanvas(figure)
-#canvas3 = FigureCanvas(figure)
-newQWidget = QWidget()
-ax = figure.add_subplot(111)
-canvas.draw()
-canvas2.draw()
-#canvas3.draw()
-
-navtool = NavigationToolbar(canvas,newQWidget)
-
-
-v_layout.addWidget(canvas)
-newQWidget.setLayout(v_layout)
-hmainBox.addWidget(newQWidget)
-v_layout.addWidget(canvas2)
-v_layoutRight = QVBoxLayout()
-#v_layoutRight.addWidget(canvas3)
-hmainBox.addLayout(v_layout)
-hmainBox.addLayout(v_layoutRight)
-
-
-window.setWindowTitle("Readings")
 wid.setLayout(hmainBox)
 
 
-window.show()
+
+
+toolbar = QToolBar()
+mainWindow.addToolBar(toolbar)
+
+
+
+#Comports
+
+comPorts = QToolButton()
+toolbar.addWidget(comPorts)
+comPorts.setText("COM")
+
+#Comports list
+menu = QMenu()
+findComPorts(menu)
+menu.triggered.connect(partial(setComPort, menu))
+comPorts.setMenu(menu)
+comPorts.setPopupMode(QToolButton.InstantPopup)
+
+
+#Other toolbar buttons
+start = QAction(QIcon(),"Start", mainWindow)
+toolbar.addAction(start)
+stop  = QAction(QIcon(),"Stop", mainWindow)
+toolbar.addAction(stop)
+plot  = QAction(QIcon(),"Plot", mainWindow)
+toolbar.addAction(plot)
+toolbar.actionTriggered[QAction].connect(display)
+
+
+
+
+
+
+ 
+
+leftLayout = QVBoxLayout()
+leftLayout.setContentsMargins(0,0,0,0)
+leftLayout.setSpacing(0)
+ECGFigure = Figure()
+PPGFigure = Figure()
+ECGCanvas = FigureCanvas(ECGFigure)
+PPGCanvas = FigureCanvas(PPGFigure)
+
+ecg = ECGFigure.add_subplot(111)
+ecg.set_title("ECG")
+ppg = PPGFigure.add_subplot(111)
+ppg.set_title("PPG")
+ECGCanvas.draw()
+PPGCanvas.draw()
+#canvas3.draw()
+ecgWindow = QWidget()
+ecgWindow.setContentsMargins(0,0,0,0)
+
+ecgWindow.setAutoFillBackground(True)
+ecgLayout = QVBoxLayout()
+ecgLayout.setContentsMargins(0,0,0,0)
+ecgLayout.setSpacing(0)
+ecgWindow.setLayout(ecgLayout)
+ppgWindow = QWidget()
+ppgLayout = QVBoxLayout()
+ppgLayout.setContentsMargins(0,0,0,0)
+ppgLayout.setSpacing(0)
+ppgWindow.setLayout(ppgLayout)
+
+ecgToolbar = NavigationToolbar(ECGCanvas,wid)
+ppgToolbar = NavigationToolbar(PPGCanvas,wid)
+
+splitter1 = QSplitter(Qt.Vertical)
+ecgLayout.addWidget(ECGCanvas)
+ecgLayout.addWidget(ecgToolbar)
+
+leftLayout.addWidget(splitter1)
+ppgLayout.addWidget(PPGCanvas)
+ppgLayout.addWidget(ppgToolbar)
+splitter1.addWidget(ecgWindow)
+splitter1.addWidget(ppgWindow)
+splitter1.setSizes([400,400])
+splitter1.setStyleSheet("QSplitter::handle {   background: black;}")
+splitter1.setHandleWidth(1)
+rightLayout = QVBoxLayout()
+#v_layoutRight.addWidget(canvas3)
+hmainBox.addLayout(leftLayout)
+hmainBox.addLayout(rightLayout)
+hmainBox.setSpacing(0)
+hmainBox.setContentsMargins(0,0,0,0)
+ 
+
+
+
+
+mainWindow.show()
 sys.exit(app.exec_())
